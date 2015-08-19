e833c5ba4237ec2843e83cf79e6f20e87ba4f1dd
Humberto Perez <ferdinero121312@gmail.com>
2015-08-18 18:31:45 -0700

    creating  files for templeting

--- prueba.php
+++ prueba.php
@@ -3,8 +3,8 @@
 	<head> Arriba Mexico</head>
 
 		<body>
-		  <h1>LO Mejor en Productos Mexicanos</h1>
-         <img src="/images/background-image.jpg"
+		  <h1>Lo Mejor en Productos Mexicanos</h1>
+         <img src="images/background-image.jpg"
 
 
 
